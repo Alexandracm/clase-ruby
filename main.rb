3522315bd424dbe0d67366d16d19656603e7075a
alexandra <phmb1234docs@gmail.com>
2018-09-28 16:37:03 -0500

    clase Ruby

---
+++ main.rb
@@ -0,0 +1,145 @@
+#nombre_usuario = "juan"
+#puts nombre_usuario
+
+#edad =26
+#puts edad+5 
+
+#puts 9/3
+#puts 10/3
+
+
+#puts "Hola " + nombre_usuario
+#puts "Hola  #{nombre_usuario}, bienvenido"
+#puts "Hola  #{nombre_usuario}, \"bienvenido\""
+
+#puts nombre_usuario.methods
+
+#puts "Ingresa un número"
+#numero_uno =gets.chomp.to_f
+#puts "ingresa otro numero"
+#numero_dos =gets.chomp.to_f
+#puts "la suma de estos numeros es #{ numero_uno+numero_dos}"
+#{puts "El número ingresado fue # {numero_uno}.gracias"}
+
+ 	
+#if numero_uno>numero_dos
+#puts"el numero uno es mayor que el numero dos"
+ #else
+ #	puts"el numero uno no es mayor a el numero dos"
+
+# end
+
+
+#puts"el numero uno es mayor que el numero dos"
+# elsif numero_uno==numero_dos
+ #	puts"los numeros son iguales"
+#else
+# #end
+
+
+#calificacion= gets.chomp.to_f
+#case calificacion
+	#when 5
+	#	puts "excelente"
+   # when 4
+	#puts "sobresaliente"
+   #when 3
+	#puts "Aceptable"
+   # when 2
+ 	#puts"Insuficente"
+   #when 1,0
+  #puts	"Deficiente"
+
+ # else
+  	#puts "Nota no valida"
+
+
+#end
+
+
+#frutas = ['manzana','pera','uva','anon','durazno']
+#puts frutas[1]
+ #frutas.each do |fruta|
+ #	puts fruta
+#end
+
+
+#puts frutas.sort.join (', ') 
+#puts frutas.include?('durazno')
+
+#(1..5) .each do |numero|
+	#puts"El numero vale #{numero}"
+#end
+
+#i=0
+#puts"1 vale #{i}"
+#i=i+1
+
+#end
+
+
+#begin
+	#puts "i vale#{i}"
+	#i=i+1
+#end while i<10
+
+#5.times do
+#puts "Hola"
+#end
+
+
+#5.upto(10) do |valor|
+	#puts valor
+#end
+
+
+
+
+
+#10.downto(5) do |valor|
+	#puts valor
+ #end
+
+
+#hashes
+
+persona = {"nombre" => "juan", "edad"=> 26}
+puts persona["edad"]
+
+persona.each do |clave,valor|
+	puts "la clave es #{clave} y el valor es  #{valor}"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
